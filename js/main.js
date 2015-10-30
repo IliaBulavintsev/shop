fa28c0e39d7efa5af61bdae22052cbd5a5e56fbf
IliaBulavintsev <twintipper@mail.ru>
2015-10-30 04:09:43 +0300

    clearing markup

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-
+$(function(){
+    console.log('ready');
+    $('.information-column').columnize({
+        columns : 2,
+        width : 200
+       });
+    $('a').on('click', function(e){
+        e.preventDefault();
+    })
+});
