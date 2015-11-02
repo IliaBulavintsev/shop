6e3e63dea244766897d9d0e1d7e9023f5657b690
IliaBulavintsev <twintipper@mail.ru>
2015-11-02 17:30:28 +0300

    clearing

--- js/main.js
+++ js/main.js
@@ -82,8 +82,7 @@ var Accardeon = function(){
 $(function(){
     console.log('ready');
     $('.information-column').columnize({
-        columns : 2,
-        width : 200
+        width : 500
        });
     $('a').on('click', function(e){
         e.preventDefault();
